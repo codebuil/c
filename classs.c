145cd899bc6071e74d1d801a73a1b487a5b3b318
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-29 17:11:05 +0100

    c++ sample

--- classs.c
+++ classs.c
@@ -4,9 +4,11 @@
 class starts{
    public:
     starts(int i){
-        printf("\e[42;30m start %d]",i);
+        printf("\e[42;30m start %d\n]",i);
     };
-    
+    ~ starts(){
+        printf("\e[42;30m end \n]");
+    };
 
 };
 int main(){
