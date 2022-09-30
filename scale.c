0c7a38d6d947ef857421b1ce727acee622069d21
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-09-30 17:15:16 +0100

    scale 0_10 to percent

---
+++ scale.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+   double scale0_10(double a){
+       double b=a;
+       if (a<0.00)b=0.00;
+       if(a>10.00)b=10.00;
+       b=b*10.00;
+       return b;
+   }
+int main(){
+    double a;
+    for(a=0;a<11.00;a++)printf("\e[42;30m %fu ]\n",scale0_10(a));
+    return 0;
+}
